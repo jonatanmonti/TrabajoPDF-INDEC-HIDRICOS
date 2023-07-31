0f61f0e7381159c9647d9a908b33ea8ac731b55e
jonatanmonti <jonatan.monti@hotmail.com>
2023-07-31 11:15:46 -0300

    Agrego linea de comentarios

--- TrabajoPDF-INDEC/ObraTablestaca.cs
+++ TrabajoPDF-INDEC/ObraTablestaca.cs
@@ -5,10 +5,10 @@ using System.Text;
 
 namespace TrabajoPDF_INDEC
 {
-    public class ObraTablestaca : ObraCincoItem
+    public class ObraTablestaca : ObraCincoItem //Esta clase Hereda de ObraCincoItem y es para la unica obra que solo usa tablestacado
     {
 
-        private float tablestaca;
+        private float tablestaca; //Item Tablestaca
 
         public float Tablestaca
         {
