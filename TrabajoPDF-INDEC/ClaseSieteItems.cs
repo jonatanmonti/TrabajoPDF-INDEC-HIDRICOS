2d1b80c978ab29fc2105184ac8dff29a7e73fddc
jonatanmonti <jonatan.monti@hotmail.com>
2023-07-31 11:14:54 -0300

    Agrego lineas de comentario

--- TrabajoPDF-INDEC/ClaseSieteItems.cs
+++ TrabajoPDF-INDEC/ClaseSieteItems.cs
@@ -5,10 +5,10 @@ using System.Text;
 
 namespace TrabajoPDF_INDEC
 {
-    public class ObraSieteItem : ObraCincoItem
+    public class ObraSieteItem : ObraCincoItem //Esta obra hereda de ObraCincoItem y agrega 2 item mas para las obras que usan 7 items
     {
 
-		private float hormigon;
+		private float hormigon; //Item Hormigon
 
 		public float Hormigon
 		{
@@ -16,7 +16,7 @@ namespace TrabajoPDF_INDEC
 			set { hormigon = value; }
 		}
 
-		private float acero;
+		private float acero; //Item Acero
 
 		public float Acero
 		{
